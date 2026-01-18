0b7ce93017a7f8456c3ae0650618e98c03a2c1de
Frank Stallone <frank@stallone.dev>
2026-01-18 10:37:43 -0500

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import reactHooks from "eslint-plugin-react-hooks";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ...reactHooks.configs.flat.recommended,
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
